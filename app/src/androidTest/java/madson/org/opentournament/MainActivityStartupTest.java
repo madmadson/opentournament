6307d479c6d0c3e2bb038300e0d1eaf0073eee55
Tobias Matt <matt@synyx.de>
2016-08-15 11:08:12 +0200

    remove testing teststuff

--- app/src/androidTest/java/madson/org/opentournament/MainActivityStartupTest.java
+++ app/src/androidTest/java/madson/org/opentournament/MainActivityStartupTest.java
@@ -18,8 +18,6 @@ import org.junit.runner.RunWith;
 import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
@@ -34,18 +32,6 @@ public class MainActivityStartupTest {
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule(MainActivity.class);
-
-    @Test
-    public void ensureTextChangesWork() {
-
-        // Type text and then press the button.
-        onView(withId(R.id.inputField)).perform(typeText("HELLO"), closeSoftKeyboard());
-        onView(withId(R.id.changeText)).perform(click());
-
-        // Check that the text was changed.
-        onView(withId(R.id.inputField)).check(matches(withText("Lalala")));
-    }
-
 
     @Test
     public void ensureNavigationToTournamentsManagement() {
--- app/src/main/java/madson/org/opentournament/HomeFragment.java
+++ app/src/main/java/madson/org/opentournament/HomeFragment.java
@@ -1,7 +1,5 @@
 package madson.org.opentournament;
 
-import android.content.Intent;
-
 import android.os.Bundle;
 
 import android.support.annotation.Nullable;
@@ -12,11 +10,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import android.widget.AdapterView;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
-
 
 public class HomeFragment extends Fragment {
 
@@ -25,17 +18,6 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         final View frag_view = inflater.inflate(R.layout.fragment_home, container, false);
-
-        Button button = (Button) frag_view.findViewById(R.id.changeText);
-        button.setOnClickListener(new View.OnClickListener() {
-
-                @Override
-                public void onClick(View view) {
-
-                    EditText editText = (EditText) frag_view.findViewById(R.id.inputField);
-                    editText.setText("Lalala");
-                }
-            });
 
         return frag_view;
     }

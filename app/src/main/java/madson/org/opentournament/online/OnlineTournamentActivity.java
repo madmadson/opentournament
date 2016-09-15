172012a56f273494f87e26e46d627f3dbd81f97d
Tobias Matt <tmatt@contargo.net>
2016-09-16 00:20:56 +0200

    start online tournament
    Signed-off-by: Tobias Matt <tmatt@contargo.net>

---
+++ app/src/main/java/madson/org/opentournament/online/OnlineTournamentActivity.java
@@ -0,0 +1,153 @@
+package madson.org.opentournament.online;
+
+import android.content.Intent;
+
+import android.os.Bundle;
+
+import android.support.design.widget.TabLayout;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+
+import android.support.v7.app.ActionBar;
+
+import android.util.Log;
+
+import android.view.Menu;
+
+import madson.org.opentournament.R;
+import madson.org.opentournament.domain.Game;
+import madson.org.opentournament.domain.Tournament;
+import madson.org.opentournament.organize.TournamentEventListener;
+import madson.org.opentournament.organize.TournamentRoundManagementFragment;
+import madson.org.opentournament.organize.setup.TournamentPlayerListFragment;
+import madson.org.opentournament.organize.setup.TournamentSetupFragment;
+import madson.org.opentournament.service.TournamentService;
+import madson.org.opentournament.utility.BaseActivity;
+
+
+/**
+ * Activity for tournament organiser.
+ */
+public class OnlineTournamentActivity extends BaseActivity {
+
+    public static final String EXTRA_TOURNAMENT = "tournament";
+
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+    private ViewPager mViewPager;
+
+    private TournamentRoundManagementFragment tournamentRoundManagementFragment;
+    private Tournament tournament;
+    private TournamentSetupFragment tournamentSetupFragment;
+
+    @Override
+    public boolean useTabLayout() {
+
+        return true;
+    }
+
+
+    @Override
+    protected void onDestroy() {
+
+        super.onDestroy();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // additionally to the navigation drawer, inflate the base menu into the options menu
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+
+        return true;
+    }
+
+
+    @Override
+    public boolean isDisplayHomeAsUp() {
+
+        return true;
+    }
+
+
+    @Override
+    public boolean useNavigationDrawer() {
+
+        return false;
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        Intent intent = getIntent();
+
+        Bundle extras = intent.getExtras();
+
+        tournament = (Tournament) extras.get(EXTRA_TOURNAMENT);
+
+        if (tournament != null) {
+            Log.i(this.getClass().toString(), "tournament opened with id " + tournament);
+
+            ActionBar supportActionBar = getSupportActionBar();
+
+            if (supportActionBar != null) {
+                supportActionBar.setTitle(tournament.getName());
+            }
+
+            mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+            mViewPager = (ViewPager) findViewById(R.id.container);
+            mViewPager.setAdapter(mSectionsPagerAdapter);
+
+            TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+            tabLayout.setupWithViewPager(mViewPager);
+        }
+    }
+
+    private class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        private Tournament tournamentToOrganize;
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+
+            Log.i(this.getClass().getName(),
+                "create tournament fragment: " + tournamentToOrganize + " on position: " + position);
+
+            return TournamentPlayerListFragment.newInstance(tournament);
+        }
+
+
+        @Override
+        public int getCount() {
+
+            if (tournamentToOrganize != null) {
+                return tournamentToOrganize.getActualRound() + 1;
+            } else {
+                return 0;
+            }
+        }
+
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+
+            if (position == 0) {
+                return getApplication().getResources().getString(R.string.nav_player_list);
+            } else {
+                return getApplication().getResources().getString(R.string.nav_round_tab, position);
+            }
+        }
+    }
+}
